607b20ff34672cdf4bcd243c486f1532c99115a4
jlyon87 <jml6487@gmail.com>
2017-08-23 13:32:31 -0400

    refactor out of controller

--- assets/js/todo-app.js
+++ assets/js/todo-app.js
@@ -1,63 +1,18 @@
 var App = (function controller(App, fb) {
-	var config = {
-		apiKey: "AIzaSyDjsp-w-1BvMYK4wpvnNvGLjAkECao_hEQ",
-		authDomain: "todolist-70f9b.firebaseapp.com",
-		databaseURL: "https://todolist-70f9b.firebaseio.com/",
-		storageBucket: "todolist-70f9b.appspot.com",
-	};
-
-	var todoList = [];
-	var database;
-
-	var handleSnapshot = function(snapshot) {
-		if(snapshot.val()) {
-			todoList = snapshot.val().todos;
-		} else {
-			todoList = [];
-		}
-
-		App.view.refreshList(todoList);
-	};
-
-	var observeTodos = function() {
-		database.ref().on("value", handleSnapshot);
-	};
-
-	var init = function() {
-		fb.initializeApp(config);
-		database = fb.database();
-		observeTodos();
-	};
-
-	var saveTodos = function(todoList) {
-		database.ref().set({
-			todos: todoList,
-		});
-		App.view.refreshList(todoList);
-	};
 
 	var addTodo = function() {
-		var todoText = document.getElementById("todo-text");
-		if(todoText.value !== "") {
-			todoList.push(todoText.value);
-			todoText.value = "";
-			saveTodos(todoList);
-		}
+		App.data.addTodo();
 	};
 
 	var removeTodo = function(i) {
-		var todoItem = document.getElementById("td-item-" + i);
-		todoList.splice(i, 1);
-		saveTodos(todoList);
+		App.data.remoteTodo(i);
 	};
 
 	var textInputKeyup = function(event) {
 		if(event.keyCode === 13) {
-			addTodo();
+			App.data.addTodo();
 		}
 	};
-
-	init();
 
 	App.controller = {
 		addTodo: addTodo,
